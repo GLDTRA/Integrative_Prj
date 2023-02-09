99bed92d69abe760dd9b0da043cf5dc220c11235
klebertonSantos <kleberton.ferreira@mercadolivre.com>
2023-02-09 13:40:43 -0300

    commit da Classe UsuarioContoller GetMapping

--- src/main/java/com/group3/projecteducation/controller/UsuarioContoller.java
+++ src/main/java/com/group3/projecteducation/controller/UsuarioContoller.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+//comentarario para atualizar
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
