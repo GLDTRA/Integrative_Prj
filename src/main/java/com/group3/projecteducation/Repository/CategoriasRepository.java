7f842f53b55836fd58f9a5a576a79fbcf5b6c5c3
GLDTRA <guga_floriano13@hotmail.com>
2023-02-09 15:51:54 -0300

    fix: bug

--- src/main/java/com/group3/projecteducation/Repository/CategoriasRepository.java
+++ src/main/java/com/group3/projecteducation/Repository/CategoriasRepository.java
@@ -11,9 +11,17 @@ import java.util.Optional;
 @Repository
 public interface CategoriasRepository extends JpaRepository<Categoria, Long> {
 <<<<<<< Updated upstream:src/main/java/com/group3/projecteducation/Repository/CategoriasRepository.java
+<<<<<<< Updated upstream:src/main/java/com/group3/projecteducation/Repository/CategoriasRepository.java
     public Optional<Categoria> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
     public Optional<Categoria> findAllByAreaContainingIgnoreCase(@Param("area") String area);
+=======
+
+
+    public List<Categoria> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+    public List<Categoria> findAllByAreaContainingIgnoreCase(@Param("area") String area);
+>>>>>>> Stashed changes:src/main/java/com/group3/projecteducation/repository/CategoriasRepository.java
 =======
 
 
